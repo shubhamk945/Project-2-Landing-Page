1ff46f8abc74fd2c26e729ff9b307c646fb31dc9
shubhamk945 <shubhamk6777@gmil.com>
2025-11-17 09:50:17 +0530

    Project done added gsap,render and scalling function

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
-let canvas = document.querySelector('canvas');
+function locomotive() {
+  gsap.registerPlugin(ScrollTrigger);
+ 
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true ,
+  });
+  locoScroll.on("scroll", ScrollTrigger.update);
+ 
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    },
+ 
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+ 
+    pinType: document.querySelector(".main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  ScrollTrigger.refresh();
+}
+locomotive();
+ 
+ 
+const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
-
+ 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
+ 
+ 
 window.addEventListener("resize", function () {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    render()
-})
-
-function file(index){
-        var data = `
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  render();
+});
+ 
+function files(index) {
+  var data = `
      ./male0001.png
      ./male0002.png
      ./male0003.png
@@ -313,19 +349,96 @@ function file(index){
      ./male0299.png
      ./male0300.png
 `;
- return data.split("/n")[index]
+  return data.split("\n")[index];
 }
-
+ 
 const frameCount = 300;
+ 
 const images = [];
 const imageSeq = {
-    frame : 1
+  frame: 1,
+};
+ 
+for (let i = 0; i < frameCount; i++) {
+  const img = new Image();
+  img.src = files(i);
+  images.push(img);
 }
-
-for(let i = 0; i < frameCount; i++){
-    const img = new Image()
-    img.src = file(i)
-    console.log(img.src = file(i));
-    images.push(img);
-    
+ 
+gsap.to(imageSeq, {
+  frame: frameCount - 1,
+  snap: "frame",
+  ease: `none`,
+  scrollTrigger: {
+    scrub: 0.15,
+    trigger: `#page>canvas`,
+    start: `top top`,
+    end: `600% top`,
+    scroller: `.main`,
+  },
+  onUpdate: render,
+});
+ 
+images[1].onload = render;
+ 
+function render() {
+  scaleImage(images[imageSeq.frame], context);
 }
+ 
+function scaleImage(img, ctx) {
+  var canvas = ctx.canvas;
+  var hRatio = canvas.width / img.width;
+  var vRatio = canvas.height / img.height;
+  var ratio = Math.max(hRatio, vRatio);
+  var centerShift_x = (canvas.width - img.width * ratio) / 2;
+  var centerShift_y = (canvas.height - img.height * ratio) / 2;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    img,
+    0,
+    0,
+    img.width,
+    img.height,
+    centerShift_x,
+    centerShift_y,
+    img.width * ratio,
+    img.height * ratio
+  );
+}
+ScrollTrigger.create({
+  trigger: "#page>canvas",
+  pin: true,
+  // markers:true,
+  scroller: `.main`,
+  start: `top top`,
+  end: `600% top`,
+});
+ 
+gsap.to("#page1",{
+  scrollTrigger:{
+    trigger:`#page1`,
+    start:`top top`,
+    end:`bottom top`,
+    pin:true,
+    scroller:`.main`
+  }
+})
+gsap.to("#page2",{
+  scrollTrigger:{
+    trigger:`#page2`,
+    start:`top top`,
+    end:`bottom top`,
+    pin:true,
+    scroller:`.main`
+  }
+})
+gsap.to("#page3",{
+  scrollTrigger:{
+    trigger:`#page3`,
+    start:`top top`,
+    end:`bottom top`,
+    pin:true,
+    scroller:`.main`
+  }
+})
+ 
